3e308260ed9cb344c9b7a742cb98c22605b89481
Mohammed <mommedmkn@gmail.com>
2023-02-25 16:05:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function getMiddle(s)
+{
+    let middle = Math.floor(s.length/2);
+    if (s.length % 2 == 0)
+        return s[middle-1] + s[middle];
+    else
+        return s[middle];
+}
+
+console.log(getMiddle("testing"));
